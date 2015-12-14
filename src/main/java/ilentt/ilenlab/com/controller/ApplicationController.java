3c3e6120e658ab920dc6a1ddf67d318b7f955eb9
ilentt <ilentt@ilenlab.com>
2015-12-14 17:45:01 +0700

    update project

--- src/main/java/ilentt/ilenlab/com/controller/ApplicationController.java
+++ src/main/java/ilentt/ilenlab/com/controller/ApplicationController.java
@@ -1,9 +1,9 @@
 package ilentt.ilenlab.com.controller;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Configuration;
 
-@Controller
+@Configuration
 public class ApplicationController {
     @Bean
     public OrdersController ordersController() {
--- src/main/java/ilentt/ilenlab/com/controller/OrdersController.java
+++ src/main/java/ilentt/ilenlab/com/controller/OrdersController.java
@@ -1,37 +1,51 @@
 package ilentt.ilenlab.com.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import ilentt.ilenlab.com.service.OrdersService;
+import ilentt.ilenlab.com.dom.Orders;
+import ilentt.ilenlab.com.service.IDummyDataService;
+import ilentt.ilenlab.com.service.IOrdersService;
 
 @Controller
 @RequestMapping(value = "/orders")
+@SessionAttributes(types = Orders.class)
 public class OrdersController {
     @Value("${application.message}")
     private String message;
     @Autowired
-    private OrdersService orderService;
+    private IOrdersService orderService;
+    @Autowired
+    private IDummyDataService dummyDataService;
 
-    /*
-     * @Autowired private IDummyDataService dummyDataService;
-     */
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView getOrders() {
-        // dummyDataService.setupOrders();
-        // List<Orders> orders = orderService.findAllOrders();
-        // return new ModelAndView("orders-list", "orders", orders);
-        return null;
+        dummyDataService.setupOrders();
+        List<Orders> orders = orderService.findAllOrders();
+        return new ModelAndView("orders-list", "orders", orders);
     }
-    /*
-     * @RequestMapping(params = "/create", method = RequestMethod.POST) public String createOrders(@Valid Orders order,
-     * BindingResult result, SessionStatus status, RedirectAttributes redirect) { if (result.hasErrors()) { return
-     * "order-edit"; } Orders orders = orderService.createOrders(order); status.setComplete();
-     * redirect.addFlashAttribute("msg", String.format("Order '%s' added successfuly", orders.getOrderID())); return
-     * "redirect:orders/" + orders.getOrderID(); }
-     */
+
+    @RequestMapping(params = "/create", method = RequestMethod.POST)
+    public String createOrders(@Valid Orders order, BindingResult result, SessionStatus status,
+            RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            return "order-edit";
+        }
+        Orders orders = orderService.createOrders(order);
+        status.setComplete();
+        redirect.addFlashAttribute("msg", String.format("Order '%s' added successfuly", orders.getOrderID()));
+        return "redirect:orders/" + orders.getOrderID();
+    }
 }
